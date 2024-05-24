bd1505653c94a7af08cd276f68ba7a992f91a84b
Hemanth Kumar M <083hemanth@gmail.com>
2024-05-24 19:04:24 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// Database configuration
+    define('HOSTNAME', 'localhost:3306');
+    define('USERNAME', 'root');
+    define('PASSWORD', 'Hemanth083@Kumar');
+    define('DATABASE', 'wt');
+?>
